242331ab6632a8391794fa9a4bdb5ae84c29f7b7
cnlucke <cristy.lucke@gmail.com>
2018-03-31 13:14:49 -0400

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  state = {
+    text: ''
+  }
+
+  handleOnChange = (e) => {
+    this.setState({text: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    console.log("submitting")
+    e.preventDefault()
+    this.props.store.dispatch({
+      type: 'ADD_RESTAURANT',
+      text: this.state.text,
+    })
+    this.setState({text: ''})
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleOnChange} value={this.state.text}></input>
+          <input type='submit'></input>
+        </form>
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,25 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+let restaurantId = 0;
+let reviewId = 0;
+
+export default function manageRestaurants(state = {restaurants: [], reviews: []}, action) {
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
+      restaurantId++;
+      const restaurants = [...state.restaurants, { id: restaurantId, text: action.text }]
+      return Object.assign({}, state, {restaurants: restaurants})
+    case 'DELETE_RESTAURANT':
+      return Object.assign({}, state, {restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.id)})
+    case 'ADD_REVIEW':
+      reviewId++
+      const reviews = [...state.reviews, { id: reviewId, text: action.text, restaurantId: action.restaurantId }]
+      return Object.assign({}, state, {reviews: reviews})
+    case 'DELETE_REVIEW':
+      return Object.assign({}, state, {reviews: state.reviews.filter(review => review.id !== action.id)})
+    default:
+      return state
+  }
 
 }
